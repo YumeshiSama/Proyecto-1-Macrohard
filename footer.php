5d4d9673a6ed0f25df4e0e6b7075dbe4018a40f6
YumeshiSama <alejo.2002@live.com.ar>
2021-09-06 20:15:56 -0300

    restar

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer class=" bg-light" style="position: absolute; bottom: 0; width: 100%;">
-
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2021 Copyright:
-    <a href="https://mdbootstrap.com/"> Macrohard Onfire </a>
-  </div>
-  <!-- Copyright -->
-  </footer>
-</footer>
--- header.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
-<script src="Bootstrap/js/jquery.min.js"></script>
-<script src="Bootstrap/js/bootstrap.min.js"></script>
