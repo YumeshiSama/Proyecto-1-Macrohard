965e58d1dd6a9be30e51ead7a0b0eb48534abec6
YumeshiSama <alejo.2002@live.com.ar>
2021-09-13 14:14:42 -0300

    Re inicio

--- server/router.php
+++ server/router.php
@@ -3,7 +3,7 @@ function router($method, $url, $closure) {
     //Request uri trae la uri actual
 	$route = $_SERVER['REQUEST_URI'];
     //Se elimina todo lo que no es la uri base para usar url's relativas
-    $route = str_replace("github/Proyecto-1-Macrohard/server/", "", $route);
+    $route = str_replace("proyectos/7mo/boilerplate_front_back/server/", "", $route);
 	if (strpos($route, '?')) {
 		$route = strstr($route, '?', true);
 	}
